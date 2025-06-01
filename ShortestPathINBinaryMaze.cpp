7f719dd663edcdab8ebc92b85c13d357e35ca529
Abhishek-TheRealCoder <abhisheksingh11102003@gmail.com>
2025-06-01 22:11:29 +0530

    Shortest Path in Binary Maze BFS(Dijkstra) using queue

--- ShortestPathINBinaryMaze.cpp
+++ ShortestPathINBinaryMaze.cpp
@@ -3,11 +3,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPoss(int i, int j , int n, int m ,vector<vector<int>>&visited,vector<vector<int>> &grid){
+    if(i >= 0 && i < n && j >= 0 && j < m && visited[i][j] == 0 && grid[i][j] == 1){
+        return true;
+    }
+    return false;
+}
+
 int shortestPathInBinaryMaze(vector<vector<int>> &grid, pair<int, int> source,pair<int, int> destination){
     int n = grid.size();
     int m = grid[0].size();
     int startRow = source.first;
     int startCol = source.second;
+    
+    int destRow = destination.first;
+    int destCol = destination.second;
 
     vector<vector<int>>visited(n,vector<int>(m,0));
 
@@ -20,7 +30,44 @@ int shortestPathInBinaryMaze(vector<vector<int>> &grid, pair<int, int> source,pa
         int dist = q.front().second;
         q.pop();
         
+        if(row == destRow && col == destCol){
+            return dist;
+        }
+        
+        //north
+        int x = row - 1;
+        int y = col ;
+        if(isPoss(x,y,n,m,visited,grid)){
+            q.push(make_pair(make_pair(x,y),dist + 1));
+            visited[x][y] = 1;
+        }
+        
+        //south 
+        x = row + 1;
+        y = col ;
+        if(isPoss(x,y,n,m,visited,grid)){
+            q.push(make_pair(make_pair(x,y),dist + 1));
+            visited[x][y] = 1;
+        }
+        
+        //east
+        x = row;
+        y = col - 1;
+        if(isPoss(x,y,n,m,visited,grid)){
+            q.push(make_pair(make_pair(x,y),dist + 1));
+            visited[x][y] = 1;
+        }
+        
+        //west
+        x = row;
+        y = col + 1;
+        if(isPoss(x,y,n,m,visited,grid)){
+            q.push(make_pair(make_pair(x,y),dist + 1));
+            visited[x][y] = 1;
+        }
+    
     }
+    return -1;
 }
 
 
