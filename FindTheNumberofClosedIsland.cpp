ba09c44f8341e25d2b41912591c419cc9a4109ed
Abhishek-TheRealCoder <abhisheksingh11102003@gmail.com>
2025-06-21 00:12:23 +0530

    (Hard Level) find the number of Closed Islands

--- FindTheNumberofClosedIsland.cpp
+++ FindTheNumberofClosedIsland.cpp
@@ -9,91 +9,73 @@ class Graph{
    }
 
    void dfs(int i, int j,int n,int m,vector<vector<int>>&matrix,vector<vector<int>>&visited){
-	visited[i][j] = 1;
+        visited[i][j] = 1;
 
-	int x = i + 1;
-	int y = j;
-	if(isPoss(x,y,n,m,matrix,visited)){
-	    dfs(x,y,n,m,matrix,visited);
-	}
-
-	x = i - 1;
-	y = j;
-	if(isPoss(x,y,n,m,matrix,visited)){
+        int x = i + 1;
+        int y = j;
+        if(isPoss(x,y,n,m,matrix,visited)){
             dfs(x,y,n,m,matrix,visited);
         }
 
-	x = i;
+        x = i - 1;
+        y = j;
+        if(isPoss(x,y,n,m,matrix,visited)){
+            dfs(x,y,n,m,matrix,visited);
+        }
+
+        x = i;
         y = j - 1;
         if(isPoss(x,y,n,m,matrix,visited)){
             dfs(x,y,n,m,matrix,visited);
         }
 
-	x = i;
+        x = i;
         y = j + 1;
         if(isPoss(x,y,n,m,matrix,visited)){
             dfs(x,y,n,m,matrix,visited);
         }
-	return;
+        return;
 
    }
    int closedIslands(vector<vector<int>>& matrix, int n, int m){
-	
-        vector<vector<int>>visited(n,vector<int>(m,0));
-	for(int i = 0;i < n;i++){
-	   for(int j = 0;j < m;j++){
-		if(i == 0 || i == n-1 || j == 0 ||  j == m-1){
-		   if(matrix[i][j] == 1 && visited[i][j] == 0){
-		       dfs(i,j,n,m,matrix,visited);	
-		   }
-		}
-	   }
-	}
 
-	int count = 0;
-	for(int i = 0;i < n;i++){
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        for(int i = 0;i < n;i++){
+           for(int j = 0;j < m;j++){
+                if(i == 0 || i == n-1 || j == 0 || j == m-1){
+                    if(matrix[i] == 1 && visited[i][j] == 0){
+                        dfs(i,j,n,m,matrix,visited);
+                    }
+                }
+                
+           }
+        }
+
+        int count = 0; 
+        for(int i = 0;i < n;i++){
           for(int j = 0;j < m;j++){
-                if(matrix[i][j] == 1 && visited[i][j] == 0){
-                     count++;
-		     dfs(i,j,n,m,matrix,visited);
+                if(matrix[i] == 1 && visited[i][j] == 0){
+                    count++;
+                    dfs(i,j,n,m,matrix,visited);
                 }
            }
         }
 
-	return count;
+        return count;
 
    }
 };
 int main(){
-  
-   vector<vector<int>>matrix = 
-          {{0, 0, 0, 0, 0, 0, 0, 1}, 
-           {0, 1, 1, 1, 1, 0, 0, 1}, 
-           {0, 1, 0, 1, 0, 0, 0, 1}, 
-           {0, 1, 1, 1, 1, 0, 1, 0}, 
-	   {1, 0, 0, 0, 0, 1, 0, 1}};
+
+   vector<vector<int>>matrix =
+          {{0, 0, 0, 0, 0, 0, 0, 1},
+           {0, 1, 1, 1, 1, 0, 0, 1},
+           {0, 1, 0, 1, 0, 0, 0, 1},
+           {0, 1, 1, 1, 1, 0, 1, 0},
+           {1, 0, 0, 0, 0, 1, 0, 1}};
    int n = matrix.size();
    int m = matrix[0].size();
    Graph obj;
-   int ans = obj.closedIslands(matrix,n,m);	
+   int ans = obj.closedIslands(matrix,n,m);
    cout<<ans;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
