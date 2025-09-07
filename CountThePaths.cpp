f75c6ed141ec41e87ed2a0e1b01fb3a15c08be7a
Abhishek-TheRealCoder <abhisheksingh11102003@gmail.com>
2025-09-07 12:48:43 +0530

    DFS + DP Medium Level-> Count the paths

---
+++ CountThePaths.cpp
@@ -0,0 +1,56 @@
+// Given a Directed Acyclic Graph (DAG) with V nodes labeled from 0 to V-1, and a list of directed edges, count the 
+// total number of distinct paths from a given start node to a destination node. Each edge is represented as 
+// edges[i] = [u, v], indicating a directed edge from u to v.
+
+// Examples :
+// Input: edges[][] = [[0,1], [0,3], [2,0], [2,1], [1,3]], V = 4, src = 2, dest = 3
+// Output: 3
+// Explanation: There are three ways to reach at 3 from 2. These are: 2 -> 1 -> 3, 2 -> 0 -> 3 and 2 -> 0 -> 1 -> 3.
+// Print-all-paths-1
+
+// Input: edges[][] = [[0,1], [1,2], [1,3], [2,3]], V = 4, src = 0, dest = 3
+// Output: 2
+// Explanation: There is two way to reach at 3 from 0 that is : 0 -> 1 -> 2 -> 3 and 0 -> 1 -> 3.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int dfs(int node,int dest,vector<vector<int>>&adj,vector<int>&dp){
+        
+    if(node == dest){
+        return 1;
+    }
+    if(dp[node] != -1) return dp[node];
+    
+    int count = 0;
+    
+    for(auto neigh : adj[node]){
+        count += dfs(neigh,dest,adj,dp);
+    }
+    
+    return dp[node] = count;
+}
+int countPaths(vector<vector<int>>& edges, int V, int src, int dest) {
+    vector<vector<int>>adj(V);
+    for(vector<int>edge:edges){
+        int u = edge[0];
+        int v = edge[1];
+        adj[u].push_back(v);
+    }
+    vector<int>dp(V,-1);
+    
+    return dfs(src,dest,adj,dp);
+}
+
+
+int main(){
+    vector<vector<int>>edges = {
+        {0,1},{0,3},{2,0},{2,1},{1,3}
+    };
+    int V = 4;
+    int src = 2;
+    int dest = 3;
+    int ans = countPaths(edges,V,src,dest);
+    cout<<ans;
+    return 0;
+}
